bb9d1f62c9ec7a26bbebd22b3830cd263cf364ac
Nicolas Limpert <limpert@fh-aachen.de>
2018-10-23 17:01:34 +0800

    hokuyo: add param handling

--- src/hokuyo_safety_zone.py
+++ src/hokuyo_safety_zone.py
@@ -6,25 +6,28 @@ from sensor_msgs.msg import LaserScan
 global filtered_data
 filtered_data = None
 global state
+global min_range, max_range
 
 def callback(data):
     global filtered_data
     filtered_data = copy.deepcopy(data)
     filtered_data.ranges = []
+    global min_range, max_range
     for i in range(len(data.ranges)):
        laser_range = data.ranges[i]
-       if laser_range < 1.0 and laser_range > 0.05:
+       if laser_range < max_range and laser_range > min_range:
            filtered_data.ranges.append(laser_range)
        else:
            filtered_data.ranges.append(0.0)
-
-
 
 
 def listener():
     rospy.init_node('scan_listener', anonymous=True)
     rate = rospy.Rate(10) # 10hz
 
+    global min_range, max_range
+    min_range = rospy.get_param("min_range", 0.5)
+    max_range = rospy.get_param("max_range", 1.0)
     pub = rospy.Publisher('/scan_filtered', LaserScan, queue_size=10)
     rospy.Subscriber("/scan", LaserScan, callback)
 
