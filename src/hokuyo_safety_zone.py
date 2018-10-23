e5e11b7d72966622c3f38eb18354181884f0de78
Nicolas Limpert <limpert@fh-aachen.de>
2018-10-23 16:41:05 +0800

    add laser filtering

--- src/hokuyo_safety_zone.py
+++ src/hokuyo_safety_zone.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 import rospy
+import copy
 from sensor_msgs.msg import LaserScan
 
+global pub
+
 def callback(data):
+    filtered_data = copy.deepcopy(data)
+    filtered_data.ranges = []
     for i in range(len(data.ranges)):
        laser_range = data.ranges[i]
        if laser_range < 1.0 and laser_range > 0.05:
-           rospy.loginfo("scan %i range: %f", i, laser_range)
+           filtered_data.ranges.append(laser_range)
+       else:
+           filtered_data.ranges.append(0.0)
+    global pub
+    pub.publish(filtered_data)
+
+
 
 
 def listener():
 
     rospy.init_node('scan_listener', anonymous=True)
 
+    global pub
+    pub = rospy.Publisher('/scan_filtered', LaserScan, queue_size=10)
     rospy.Subscriber("/scan", LaserScan, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
